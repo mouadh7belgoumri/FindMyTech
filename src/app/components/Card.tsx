a106fa4010ddd05dcc14781032b279e38b7de9e6
mouadh7belgoumri <m_belgoumri@estin.dz>
2025-04-12 18:24:44 +0100

    finalisation des cartes

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -12,7 +12,7 @@ export default function Card() {
             <p className="card-text bg-white p-1 px-2 rounded-lg">computer component</p>
 
           </div>
-          <div className="flex flex-row justify-around items-center w-full  ">
+          <div className="flex flex-row justify-between px-4 items-center w-full  ">
             <h1 className='text-black mt-2  text-xl card-title  '>Product name</h1>
             <div className='text-black'>
               <p className='inline-block text-center align-middle mt-2'>4.8 </p>
@@ -20,7 +20,7 @@ export default function Card() {
 
             </div>
           </div>
-          <div className="flex flex-row justify-between items-end h-full w-full mt-4 px-3 mb-2 m-md ">
+          <div className="flex flex-row justify-between items-end h-full w-full mt-4 pl-4 pr-1 mb-2 m-md ">
             <p className="text-black price font-semibold">$199.99</p>
             <button className="bg-[#D9D9D9] text-black button button-text px-4 py-2 rounded-4xl hover:bg-blue-600 hover:cursor-pointer hover:text-white hover:transition-all hover:duration-300">
               Add to Cart
